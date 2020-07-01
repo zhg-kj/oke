b89df9d1e6e8a02eb6bf6adde60ef20c7cf74183
Kai Jun Zhuang <eddieezhuang@gmail.com>
2020-06-30 20:43:29 -0700

    home page styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import {ReactComponent as EighthNote} from "./svgs/eighthNote.svg"
 import './App.css';
 
 class App extends Component{
   render(){
     return(
-      <h1>Nice</h1>
+      <div>
+        <div className="homeHeader">
+          <h1><strong>SING WITH FRIENDS.</strong></h1>
+          <h4>ANYTIME, AT THE TOUCH OF YOUR FINGERTIPS</h4>
+          <EighthNote className="eighthNote"/> 
+        </div>
+      </div>
     );
   }
 }
